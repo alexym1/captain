addd97bd72951836fe5bd488c84d37181b7412be
Alex MARTINEZ <yahiaoui-martinez.alex@outlook.com>
2025-08-23 18:03:48 +0200

    style the pkg

--- tests/testthat/test_create_hook.R
+++ tests/testthat/test_create_hook.R
@@ -4,11 +4,11 @@ library(withr)
 test_that("Testing create_hook_script()", {
   with_tempdir({
     dir.create("inst/pre-commit/hooks", recursive = TRUE)
-    create_hook_script(name = 'tmp_hook')
+    create_hook_script(name = "tmp_hook")
     expect_true(file.exists("inst/pre-commit/hooks/tmp_hook.R"))
   })
 })
 
 test_that("Testing create_hook()", {
-  create_hook(id="test_hook", name = 'tmp_hook', description = "A temporary hook")
+  create_hook(id = "test_hook", name = "tmp_hook", description = "A temporary hook")
 })
