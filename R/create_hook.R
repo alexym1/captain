364669a224ac412f398b5efdb5b84ba14f181b80
Alex MARTINEZ <yahiaoui-martinez.alex@outlook.com>
2025-08-23 13:33:06 +0200

    Complete create_hook.R

--- R/create_hook.R
+++ R/create_hook.R
@@ -1,16 +1,16 @@
 #' Create a pre-commit hook
 #'
 #' Add a pre-commit hook to .pre-commit-config.y*ml
-#' 
+#'
 #' @param id The unique identifier for the hook.
 #' @param name A descriptive name for the hook.
 #' @param description A brief description of what the hook does.
 #' @param language The programming language or environment for the hook (default is "system").
 #' @param always_run Logical, whether the hook should always run (default is TRUE).
-#' 
+#'
 #' @importFrom yaml verbatim_logical write_yaml  yaml.load_file
 #' @importFrom fs file_exists
-#' @importFrom cli cli_alert_danger cli_div
+#' @importFrom cli cli_alert_danger cli_alert_success cli_div
 #'
 #' @export
 create_hook <- function(id, name, description, language = "system", always_run = TRUE) {
@@ -19,17 +19,23 @@ create_hook <- function(id, name, description, language = "system", always_run =
 
   cli_div(theme = list(span.emph = list(color = "orange")))
 
-  if(all(found_files)) {
+  if (all(found_files)) {
     cli_alert_danger("Multiple pre-commit files are found. Keep one file and re-run `create_hook(...)`.")
     return(invisible())
-  } else if(all(!found_files)){
+  } else if (all(!found_files)) {
     cli_alert_danger("{.emph pre-commit} doesn't exist. Run `install_precommit()`.")
     return(invisible())
   }
 
+  # Create the hook script
+  create_hook_script(name = name)
+  cli_alert_success("{.emph inst/pre-commit/hooks/{name}.R} has been successfully created.")
+
+
+  # Update the config file
   config_file <- files[found_files]
   config <- yaml.load_file(config_file)
-  
+
   new_hook <- list(
     id = id,
     name = name,
@@ -40,19 +46,37 @@ create_hook <- function(id, name, description, language = "system", always_run =
     always_run = always_run
   )
 
-  # Automate the create of the R script relatedto the hook
-  # Open this R script in the positron editor + create a template for the hook
-
   config$repos[[1]]$hooks <- append(config$repos[[1]]$hooks, list(new_hook))
-  write_yaml(config, config_file, handlers = list(logical=verbatim_logical), indent = 4)
-  cli_alert_danger("{.emph {config_file}} has been created.")
-
-  
-
-  cli_alert_danger("{.emph Hook {new_hook$id}} has been created.")
+  write_yaml(config, config_file, handlers = list(logical = verbatim_logical), indent = 4)
+  cli_alert_success("{.emph {config_file}} has been successfully updated. Edit with `edit_precommit_config()`")
 }
 
 
-template_precommit_hook <- function() {
-  
+create_hook_script <- function(name) {
+  path_script <- paste0("inst/pre-commit/hooks/", name, ".R")
+  file.create(path_script)
+  writeLines(.template_content(), path_script)
+  Sys.chmod(path_script, mode = "0755", use_umask = TRUE)
+}
+
+
+.template_content <- function() {
+  template <- c(
+    "#!/usr/bin/env Rscript",
+    "",
+    "# cli::cli_h1(\"Describe the goal of your hook\")",
+    "",
+    "# Add your hook logic here",
+    "",
+    "# If success return status 0",
+    "# if (condition) {",
+    "#   quit(save = \"no\", status = 0, runLast = FALSE)",
+    "# }",
+    "",
+    "# If failed return status 1",
+    "# quit(save = \"no\", status = 1, runLast = FALSE)",
+    "",
+    "# Get more inspiration at https://github.com/alexym1/Rprecommit/tree/master/inst/pre-commit/hooks"
+  )
+  return(template)
 }
